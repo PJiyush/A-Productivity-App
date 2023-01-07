45aac5b42378fc3c107137c7de1a4039ce7bb9a3
PJiyush <jhamnani.1@iitj.ac.in>
2023-01-07 22:43:36 +0530

    Added a Navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div >
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid ">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item ">
+                                <a className="nav-link float-right" href="#">Pomodoro</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
